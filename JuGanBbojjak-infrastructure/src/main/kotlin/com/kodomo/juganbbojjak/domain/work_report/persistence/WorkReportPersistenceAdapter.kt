6513352252dad52fdabe80f05712cd2824a96245
woonseok105 <yws3786@gmail.com>
2023-12-12 21:08:33 +0900

    feat :: 주간 업무보고 상세조회 api

--- JuGanBbojjak-infrastructure/src/main/kotlin/com/kodomo/juganbbojjak/domain/work_report/persistence/WorkReportPersistenceAdapter.kt
+++ JuGanBbojjak-infrastructure/src/main/kotlin/com/kodomo/juganbbojjak/domain/work_report/persistence/WorkReportPersistenceAdapter.kt
@@ -1,15 +1,20 @@
 package com.kodomo.juganbbojjak.domain.work_report.persistence
 
 import com.kodomo.juganbbojjak.domain.work_report.model.WorkReport
+import com.kodomo.juganbbojjak.domain.work_report.persistence.entity.QWorkReportEntity.workReportEntity
 import com.kodomo.juganbbojjak.domain.work_report.persistence.mapper.WorkReportMapper
 import com.kodomo.juganbbojjak.domain.work_report.persistence.repository.WorkReportRepository
 import com.kodomo.juganbbojjak.domain.work_report.spi.WorkReportPort
 import com.kodomo.juganbbojjak.global.annotation.Adapter
+import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.jpa.impl.JPAQueryFactory
+import java.util.*
 
 @Adapter
 class WorkReportPersistenceAdapter(
     private val workReportRepository: WorkReportRepository,
-    private val workReportMapper: WorkReportMapper
+    private val workReportMapper: WorkReportMapper,
+    private val queryFactory: JPAQueryFactory
 ) : WorkReportPort {
 
     override fun saveWorkReport(workReport: WorkReport): WorkReport =
@@ -18,5 +23,29 @@ class WorkReportPersistenceAdapter(
                 workReportMapper.toEntity(workReport)
             )
         )
+
+    override fun queryWorkReportByWeeklyWorkReportId(weeklyWorkReportId: UUID, userId: UUID?): WorkReport? {
+        val workReportEntity = queryFactory
+            .selectFrom(workReportEntity)
+            .where(
+                workReportEntity.id.eq(weeklyWorkReportId),
+                eqUserId(userId)
+            )
+            .fetchOne()
+
+        return workReportEntity?.let {
+            WorkReport(
+                id = it.id,
+                title = it.title,
+                weeklyWorkReportId = weeklyWorkReportId,
+                userId = it.userEntity.id
+            )
+        }
+    }
+
+    private fun eqUserId(userId: UUID?): BooleanExpression? =
+        if (userId != null)
+            workReportEntity.userEntity.id.eq(userId)
+        else null
 
 }
